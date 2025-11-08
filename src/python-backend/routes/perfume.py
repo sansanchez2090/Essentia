d5cf6636dd92a67c543555392d9d962e28cf3044
sansanchez2090 <san.sanchezm.2090@gmail.com>
2025-11-08 13:21:30 -0500

    refactor(api): move perfume endpoints to his own file

---
+++ src/python-backend/routes/perfume.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, Depends, HTTPException, APIRouter # type: ignore
+from sqlalchemy.orm import Session # type: ignore
+import models, schemas
+from db import SessionLocal, engine
+from typing import Annotated
+from db import get_db
+
+
+router = APIRouter()
+
+@router.get("/perfumes")
+async def get_perfumes(db: Annotated[Session, Depends(get_db)]):
+    """
+    Retrieve all perfumes
+    """
+    perfumes = db.query(models.Perfume).all()
+    return perfumes
+
+@router.get("/perfumes/{perfume_id}")
+async def get_perfumes_by_id(perfume_id: int, db: Annotated[Session, Depends(get_db)]):
+    """
+    Retrieve a perfume by it's id 
+    """
+    perfume = db.query(models.Perfume).filter(models.Perfume.id == perfume_id).first()
+    if perfume is None:
+        raise HTTPException(status_code=404, detail=f"Perfume with ID: {perfume_id} not found")
+    return perfume
+
+@router.post("/perfumes")
+def create_perfume(perfume: schemas.PerfumeBase, db: Annotated[Session, Depends(get_db)]):
+    """
+    create a perfume
+    """
+    db_perfume = models.Perfume(name=perfume.name,
+                                description=perfume.description,
+                                release_year=perfume.release_year,
+                                gender=perfume.gender,
+                                image_url=perfume.image_url)
+    db.add(db_perfume)
+    db.commit()
+    db.refresh(db_perfume)
+
+@router.delete("/perfumes/{perfume_id}")
+async def delete_perfume(perfume_id: int, db: Annotated[Session, Depends(get_db)]):
+    """
+    delete a perfume
+    """
+    perfume = db.query(models.Perfume).filter(models.Perfume.id == perfume_id).first()
+    if perfume is None:
+        raise HTTPException(status_code=404, detail=f"Perfume with ID: {perfume_id} not found")
+    db.delete(perfume)
+    db.commit()
+    return {"detail": "Perfume deleted successfully"}
+
+@router.put("/perfumes/{perfume_id}")
+async def update_perfume(perfume_id: int, 
+                         updated_perfume: schemas.PerfumeBase, 
+                         db: Annotated[Session, Depends(get_db)]):
+    """
+    update a perfume
+    """
+    
+    perfume_query = db.query(models.Perfume).filter(models.Perfume.id == perfume_id).first()
+
+    if perfume_query is None:
+        raise HTTPException(status_code=404, detail=f"Perfume with ID: {perfume_id} not found")
+    
+    perfume_data = updated_perfume.model_dump(exclude_unset=True)  # Use exclude_unset to avoid updating fields that were not provided
+
+    for key, value in perfume_data.items():
+        setattr(perfume_query, key, value)
+
+    db.commit()
+
+    db.refresh(perfume_query)
+
+    return {"data": perfume_query}
